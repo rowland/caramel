338f38fe8158f0cc443d371e91d950f4c09cf89b
Brent Rowland <brent.rowland@ttiltd.com>
2019-07-05 13:37:28 -0700

    Verify application of styles based on dynamic classes.

--- spec/widgets/document_spec.cr
+++ spec/widgets/document_spec.cr
@@ -4,13 +4,17 @@ include Caramel::Widgets
 
 describe Caramel::Widgets::Document do
   describe "#new" do
+    doc = Document.new(File.join(__DIR__, "document_spec.crml"))
+
     it "should initialize itself and children from XML document" do
-      doc = Document.new(File.join(__DIR__, "document_spec.crml"))
       doc.pages.size.should eq 2
       doc.pages[0].widgets.size.should eq 1
       doc.pages[1].widgets.size.should eq 1
       doc.pages[1].widgets[0].as(Container).widgets.size.should eq 1
-      # doc.node.xpath_node("//p")["class"].should contain("basic-latin")
+    end
+
+    it "should apply styles based on dynamic classes" do
+      doc.pages[0].widgets[0].node["font"].should eq "Courier"
     end
   end
 end
