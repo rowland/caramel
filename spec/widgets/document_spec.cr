73eee7a985381c3aa3c5311234fab2b73a967517
Brent Rowland <brent.rowland@ttiltd.com>
2019-07-06 15:35:35 -0700

    Stop polluting global namespace.

--- spec/widgets/document_spec.cr
+++ spec/widgets/document_spec.cr
@@ -1,20 +1,20 @@
 require "./spec_helper"
 
-include Caramel::Widgets
+module Caramel::Widgets
+  describe Document do
+    describe "#new" do
+      doc = Document.new(File.join(__DIR__, "document_spec.crml"))
 
-describe Caramel::Widgets::Document do
-  describe "#new" do
-    doc = Document.new(File.join(__DIR__, "document_spec.crml"))
+      it "should initialize itself and children from XML document" do
+        doc.pages.size.should eq 2
+        doc.pages[0].widgets.size.should eq 1
+        doc.pages[1].widgets.size.should eq 1
+        doc.pages[1].widgets[0].as(Container).widgets.size.should eq 1
+      end
 
-    it "should initialize itself and children from XML document" do
-      doc.pages.size.should eq 2
-      doc.pages[0].widgets.size.should eq 1
-      doc.pages[1].widgets.size.should eq 1
-      doc.pages[1].widgets[0].as(Container).widgets.size.should eq 1
-    end
-
-    it "should apply styles based on dynamic classes" do
-      doc.pages[0].widgets[0].node["font"].should eq "Courier"
+      it "should apply styles based on dynamic classes" do
+        doc.pages[0].widgets[0].node["font"].should eq "Courier"
+      end
     end
   end
 end
--- spec/widgets/factory_spec.cr
+++ spec/widgets/factory_spec.cr
@@ -1,30 +1,30 @@
 require "./spec_helper"
 
-include Caramel::Widgets
-
-class Bar < Widget
-end
-
-describe Caramel::Widgets do
-  describe "#register" do
-    it "should register a tag" do
-      Factory.register("foo") { |parent, node| Widget.new(parent, node) }
-      Factory.tags.should contain("foo")
-    end
+module Caramel::Widgets
+  class Bar < Widget
   end
 
-  describe "#make" do
-    Factory.register("bar") { |parent, node| Bar.new(parent, node) }
-    doc = Document.new
-    node = XML.parse("<bar />")
-
-    it "should make a Bar widget" do
-      Factory.make("bar", doc, node).should be_a(Bar)
+  describe Caramel::Widgets do
+    describe "#register" do
+      it "should register a tag" do
+        Factory.register("foo") { |parent, node| Widget.new(parent, node) }
+        Factory.tags.should contain("foo")
+      end
     end
 
-    it "should raise an exception for an unknown tag" do
-      expect_raises(Factory::UnregisteredWidget) do
-        Factory.make("foobar", doc, node)
+    describe "#make" do
+      Factory.register("bar") { |parent, node| Bar.new(parent, node) }
+      doc = Document.new
+      node = XML.parse("<bar />")
+
+      it "should make a Bar widget" do
+        Factory.make("bar", doc, node).should be_a(Bar)
+      end
+
+      it "should raise an exception for an unknown tag" do
+        expect_raises(Factory::UnregisteredWidget) do
+          Factory.make("foobar", doc, node)
+        end
       end
     end
   end
