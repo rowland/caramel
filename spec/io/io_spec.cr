81f22a9894e19eb29adac3477054ddca7362a458
Brent Rowland <brent.rowland@ttiltd.com>
2019-07-07 18:37:06 -0700

    Put assertions the right place.

--- spec/io/io_spec.cr
+++ spec/io/io_spec.cr
@@ -8,10 +8,12 @@ describe Caramel::IO do
   http_io_spec_css = "doc page h1 { padding: 15 };"
   WebMock.stub(:get, "http://www.example.com/io_spec.css").to_return(body: http_io_spec_css)
 
-  describe "expand" do
-    Caramel::IO.expand(doc, __DIR__, "styles", "src")
-    nodes = doc.xpath_nodes("//styles")
-    nodes[0].content.should eq(io_spec_css)
-    nodes[1].content.should eq(http_io_spec_css)
+  describe "#expand" do
+    it "should expand external styles" do
+      Caramel::IO.expand(doc, __DIR__, "styles", "src")
+      nodes = doc.xpath_nodes("//styles")
+      nodes[0].content.should eq(io_spec_css)
+      nodes[1].content.should eq(http_io_spec_css)
+    end
   end
 end
