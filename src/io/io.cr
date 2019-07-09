d3ad96ce066a3a1017b674fa5a7837430346a82b
Brent Rowland <brent.rowland@ttiltd.com>
2019-07-08 23:20:17 -0700

    Factor our read_uri for reuse.

--- src/io/io.cr
+++ src/io/io.cr
@@ -8,21 +8,25 @@ module Caramel::IO
   RE_HTTP_URI = /^https?:\/\//i
   RE_FILE_URI = /^file:\/\/\//i
 
+  def read_uri(uri : String, base_uri : String) : String
+    full_uri = File.join(base_uri, uri)
+    case
+    when uri =~ RE_HTTP_URI
+      HTTP::Client.get(uri).body
+    when uri =~ RE_FILE_URI
+      File.read(uri.sub(RE_FILE_URI, ""))
+    when full_uri =~ RE_HTTP_URI
+      HTTP::Client.get(full_uri).body
+    when full_uri =~ RE_FILE_URI
+      File.read(full_uri.sub(RE_FILE_URI, ""))
+    else
+      File.read(full_uri)
+    end
+  end
+
   def expand_element(element : XML::Node, base_uri : String, attr : String)
     if uri = element[attr]?
-      full_uri = File.join(base_uri, uri)
-      element.content = case
-                        when uri =~ RE_HTTP_URI
-                          HTTP::Client.get(uri).body
-                        when uri =~ RE_FILE_URI
-                          File.read(uri.sub(RE_FILE_URI, ""))
-                        when full_uri =~ RE_HTTP_URI
-                          HTTP::Client.get(full_uri).body
-                        when full_uri =~ RE_FILE_URI
-                          File.read(full_uri.sub(RE_FILE_URI, ""))
-                        else
-                          File.read(File.join(base_uri, uri))
-                        end
+      element.content = read_uri(uri, base_uri)
     end
   end
 
